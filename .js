a536da387810523903521419a399db42bf8666cd
aryanmalik567 <aryanmalik567@gmail.com>
2017-05-25 17:38:43 +0100

    Dcgs Website

---
+++ .js
@@ -0,0 +1,30 @@
+function main() {
+
+    $.fn.scrollView = function () {
+        return this.each(function () {
+            $('html, body').animate({
+                scrollTop: $(this).offset().top
+            }, 1000);
+        });
+    }
+
+
+    $('#diary').click(function (event) {
+        event.preventDefault();
+        $('#goHere').scrollView();
+    });
+
+    $('.submenu').hide();
+    $('.MoreButton').click(function () {
+        $('.MoreButton').show();
+        $('.submenu').slideToggle();
+        $(this).toggleClass('active');
+    });
+};
+
+$(document).ready(main);
+
+
+
+
+
